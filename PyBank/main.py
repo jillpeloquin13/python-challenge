ac58fbdc963c111bdd61f902c704c9ac12cf2d14
Jill Peloquin <jill.peloquin13@gmail.com>
2021-06-18 17:17:20 -0400

    adding logic for open file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,25 @@
 # PyBank main.py
+
+# Your task is to create a Python script that analyzes the records to calculate each of the following:
+# The total number of months included in the dataset
+# he net total amount of "Profit/Losses" over the entire period
+# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in profits (date and amount) over the entire period
+
+import os
+import csv
+
+#get the path, remember that this is with respect to where the python script is found 
+csvpath = os.path.join('Resource', '02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')
+
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csv, delimiter=',')
+    print(csvreader)
+
+csv_header = next(csvreader)
+print(f"CSV Header: {csv_header}")
+
+for row in csvreader:
+    print(row)
