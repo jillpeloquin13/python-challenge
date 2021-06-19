fca0d02b7e37355618d1ca2df20fc3965a7d30f6
Jill Peloquin <jill.peloquin13@gmail.com>
2021-06-19 18:03:02 -0400

    Logic place for terminal output

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,19 +8,60 @@
 
 import os
 import csv
+rows =[]
+ave_change = []
 
 #get the path, remember that this is with respect to where the python script is found 
 csvpath = os.path.join('Resource', '02-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv')
 
 with open(csvpath) as csvfile:
-
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
-    
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
+    for row in csvreader:
+        rows.append(row)
+    
+    
+  
     #count/define the number of months
-    months = len(list(csvreader))
-    print(months)
- 
+    months = len(rows)
+    
+    
+    numbertotal = 0.0
+    for row in rows:
+        numbertotal += float(row[1])
+    
+    
+    previous = 0
+    for row in rows:
+        value1 = float(row[1])
+        ave = value1-previous
+        values = [row[0], ave]
+        ave_change.append(values)
+        previous = value1
+    
+    #get rid of the first row as not valid
+    ave_change = (ave_change[1:])
+    
+    sum = 0.0
+    for row in ave_change:
+        sum += float(row[1])
+    
+    
+
+
+     # take the second element for sort
+    def take_second(elem):
+        return elem[1]
+
+    # sort list with key
+    sorted_list_ave = sorted(ave_change, key=take_second)
+    TotalIncrease = sorted_list_ave[-1]
+    TotalDecrease = sorted_list_ave[0]
+
+    print(f"Financial Analysis")
+    print("------------------------------------")
+    print(f"Total Months: {months}")
+    print(f"Total ${numbertotal}")
+    print(f"Average Change: ${sum/(months-1):5.2f}")
+    print(f"Greatest Increase in Profits:", TotalIncrease[0], "($",TotalIncrease[1],")")
+    print(f"Greated Decrease in Profits:", TotalDecrease[0], "($",TotalDecrease[1],")")
