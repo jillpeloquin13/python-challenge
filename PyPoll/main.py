57e626b1f989d00a6ca86486975b3fe33d9a6904
Jill Peloquin <jill.peloquin13@gmail.com>
2021-06-23 00:34:24 -0400

    committing v1 of pypoll with text and console output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,53 @@
 # Pypoll main.py 
+# The total number of votes cast
+# A complete list of candidates who received votes
+# The percentage of votes each candidate won
+# The total number of votes each candidate won
+# The winner of the election based on popular vote.
+
+import os
+import csv
+import operator
+from collections import Counter
+rows = []
+
+csvpath = os.path.join('Resources', '02-Homework_03-Python_Instructions_PyPoll_Resources_election_data.csv')
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    for row in csvreader:
+        rows.append(row)
+
+    #count the total number of votes cast
+    votescast = len(rows)
+    
+    # Determine unique Canididates 
+    Candidates = []
+    for row in rows:
+        Candidates.append(row[2])
+    list = (Counter(Candidates))
+
+    for key in list:
+        print(key, ':' , ('%.3f'%(int(list[key])/votescast*100)),'% (',list[key],')')
+    
+    Winner = max(list.items(), key=operator.itemgetter(1))[0]
+
+    print(f'Election Results')
+    print(f'------------------------------------')
+    print(f"Total Votes: {votescast}")
+    print(f'------------------------------------')
+    print(f"Winner: {Winner}")
+    print(f'------------------------------------')
+    
+    
+    output_path = os.path.join("polls.text")
+    with open(output_path, 'w', newline='') as f:
+        f.writelines("Election Results \n")
+        f.writelines('\n')
+        f.writelines('-------------------------------- \n')
+        f.writelines('\n')
+        f.writelines(f"Total Votes: {votescast}")
+        f.writelines(f'------------------------------------')
+        f.writelines(f"Winner: {Winner}")
+        f.writelines(f'------------------------------------')
